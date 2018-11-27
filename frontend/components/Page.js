d0bed5ab5abb06b6acea4abe68fee5dfed27f9d7
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-27 18:54:02 -0500

    feat(Page) Declare css rule for styled page

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -3,6 +3,11 @@ import Header from "./Header";
 import Meta from "./Meta";
 import styled from "styled-components";
 
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
+
 export default class Page extends Component {
   render() {
     return (
