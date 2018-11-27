ea01d363a2c86792d1131889697079dc72788ff7
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-27 15:04:55 -0500

    feat(Header) Create Header component

---
+++ frontend/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => (
+  <div>
+    <div className="bar">
+      <a href="">The Feiner Things</a>
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>
+);
+
+export default Header;
