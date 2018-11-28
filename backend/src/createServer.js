ebd2cc6e6ffad988d2bea90c2981f5987c0924a6
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-28 16:24:21 -0500

    chore(createServer): Add in some comments explaining errs

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -6,7 +6,9 @@ const db = require("./db");
 //Create the GraphQL Yoga Server
 function createServer() {
   return new GraphQLServer({
+    //Cannot be empty
     typeDefs: "src/schema.graphql",
+    //Must resolve to something
     resolvers: {
       Mutation,
       Query
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 //Gives application access to env variables
 require("dotenv").config({ path: "variables.env" });
+
 const createServer = require("./createServer");
 const db = require("./db");
 
