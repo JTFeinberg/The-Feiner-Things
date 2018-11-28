3051d54fe06d95149271415f5a6e7749f2bf19f1
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-27 19:34:07 -0500

    style(Header): Declare css rule for Logo

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,5 +1,14 @@
 import Nav from "./Nav";
 import Link from "next/link";
+import styled from "styled-components";
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+`;
 
 const Header = () => (
   <div>
