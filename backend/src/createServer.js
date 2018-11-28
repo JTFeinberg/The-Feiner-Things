192d326bd43d41eda5eaeb125973f68d9df2c0e1
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-28 15:50:40 -0500

    config(createServer): Define options for Yoga server

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -5,7 +5,17 @@ const db = require("./db");
 
 //Create the GraphQL Yoga Server
 function createServer() {
-  return new GraphQLServer({});
+  return new GraphQLServer({
+    typeDefs: "src/schema.graphql",
+    resolvers: {
+      Mutation,
+      Query
+    },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    },
+    context: req => ({ ...req, db })
+  });
 }
 
 module.exports = createServer;
