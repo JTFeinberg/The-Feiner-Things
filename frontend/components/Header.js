11b80c495509f341dfc7ccc221c61bb940660c60
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-27 19:49:03 -0500

    style(Header): Add css rul for .bar in StyledHeader

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -21,7 +21,15 @@ const Logo = styled.h1`
   }
 `;
 
-const StyledHeader = styled.header``;
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+  }
+`;
 
 const Header = () => (
   <StyledHeader>
