ee781641b19487cb605ba9596a9352519271b351
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-28 16:04:14 -0500

    feat(src/index): Declare variable for server

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,3 +2,8 @@
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
+
+const server = createServer();
+
+// TO DO Use express middleware to handle cookies
+// TO DO Use express middleware to populate current user
