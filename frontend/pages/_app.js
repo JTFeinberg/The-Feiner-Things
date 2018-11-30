21d55ba29988dda44f873a22c6fe33e85e93baaa
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-30 12:27:13 -0500

    feat(_app): Write logic for getting data for each component before render

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,6 +4,14 @@ import { ApolloProvider } from 'react-apollo'
 import withData from '../lib/withData'
 
 class MyApp extends App {
+  async static getInitialProps({ Component, ctx}) {
+    let pageProps = {}
+    if(Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+    pageProps.query = ctx.query
+    return { pageProps }
+  }
   render() {
     const { Component, apollo } = this.props
     return (
