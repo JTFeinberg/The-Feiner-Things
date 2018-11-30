1926efe4f229125b8622522928c5fe3c3d0cb50a
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-30 13:33:14 -0500

    feat(Items): Render out items in ItemList

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
+import styled from 'styled-components'
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -19,7 +20,7 @@ const Center = styled.div`
 `
 const ItemsList = styled.div`
   display: grid;
-  grid-auto-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   grid-gap: 60px;
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
@@ -29,12 +30,17 @@ export default class Items extends Component {
   render() {
     return (
       <Center>
-        <p>Items!</p>
         <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>
             if (error) return <p>Error: {error.message}</p>
-            return <p>I found {data.items.length} items</p>
+            return (
+              <ItemsList>
+                {data.items.map(item => (
+                  <p>{item.title}</p>
+                ))}
+              </ItemsList>
+            )
           }}
         </Query>
       </Center>
