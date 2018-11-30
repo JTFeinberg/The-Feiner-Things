2514f8dc64f1a5d64d675739f8bd65f41ac59f51
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-30 13:44:51 -0500

    feat(Item): Add Item styles and title styles to component

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Title from './styles/Title'
+import ItemStyles from './styles/ItemStyles'
+import PriceTag from './styles/PriceTag'
 
 export default class Item extends Component {
   static propTypes = {
@@ -7,6 +10,11 @@ export default class Item extends Component {
   }
 
   render() {
-    return <div />
+    const { item } = this.props
+    return (
+      <ItemStyles>
+        <Title>{item.title}</Title>
+      </ItemStyles>
+    )
   }
 }
