9664ea79d0d28093558d0369325d5d2d9624af19
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-30 14:37:26 -0500

    feat(CreateItem): Create scaffolding for CreateItem component

---
+++ frontend/components/CreateItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+import Form from './styles/Form'
+import formatMoney from '../lib/formatMoney'
+
+export default class CreateItem extends Component {
+  render() {
+    return <div />
+  }
+}
