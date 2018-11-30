a9761367b2c0a6766dc011652c01b9e0fe4feec3
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-30 12:28:22 -0500

    chore(_app): Comment getInitialProps lifecycle for later clarity

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,14 +4,18 @@ import { ApolloProvider } from 'react-apollo'
 import withData from '../lib/withData'
 
 class MyApp extends App {
+  //Next.js lifecycle that runs before render. Anything returned is exposed to props in render
   async static getInitialProps({ Component, ctx}) {
     let pageProps = {}
+    //Crawl every page for any mutations or queries it may have and fetch the data before component renders
     if(Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx)
     }
+    //This exposes the query to the user
     pageProps.query = ctx.query
     return { pageProps }
   }
+  
   render() {
     const { Component, apollo } = this.props
     return (
