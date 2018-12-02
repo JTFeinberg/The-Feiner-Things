0de565eea60127c2b977bbcfbc374ea0cf0a26a4
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-02 16:57:50 -0500

    feat(DeleteItem): Write filtered data back to cache

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -20,6 +20,8 @@ export default class DeleteItem extends Component {
     data.items = data.items.filter(
       item => item.id !== payload.data.deleteItem.id
     )
+    //3. Put the items back!
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data })
   }
   render() {
     return (
