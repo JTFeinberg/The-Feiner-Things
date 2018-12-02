f2fe58ea06529631c86a45e60ce6f96b53f25116
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-02 14:25:00 -0500

    feat(Query): Write resolver for querying one item

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,7 +2,8 @@ const { forwardTo } = require('prisma-binding')
 
 const Query = {
   // Since no additional logic is needed, we can just forward the query logic from prisma straight to the db
-  items: forwardTo('db')
+  items: forwardTo('db'),
+  item: forwardTo('db')
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items()
   //   return items
