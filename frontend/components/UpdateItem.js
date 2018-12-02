d5a8dd9b403fa21d986b86b8d01c50ccbfe49355
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-02 14:40:01 -0500

    feat(UpdateItem): Create copy component of CreateItem as base

---
+++ frontend/components/UpdateItem.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+import Router from 'next/router'
+import Form from './styles/Form'
+import formatMoney from '../lib/formatMoney'
+import Error from './ErrorMessage'
+
+const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $image: String
+    $largeImage: String
+    $price: Int!
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      image: $image
+      largeImage: $largeImage
+      price: $price
+    ) {
+      id
+    }
+  }
+`
+
+export default class UpdateItem extends Component {
+  state = {
+    title: '',
+    description: '',
+    image: '',
+    largeImage: '',
+    price: 0
+  }
+  handleChange = e => {
+    const { name, type, value } = e.target
+    const val = type === 'number' ? parseFloat(value) : value
+    this.setState({ [name]: val })
+  }
+  uploadFile = async e => {
+    console.log('Uploading File...')
+    const { files } = e.target
+    const data = new FormData()
+    data.append('file', files[0])
+    //Argument needed by cloudinary
+    data.append('upload_preset', 'feinerThings')
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/dbafptufn/image/upload',
+      {
+        method: 'POST',
+        body: data
+      }
+    )
+    const file = await res.json()
+    console.log(file)
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    })
+  }
+  render() {
+    const { title, description, price, image } = this.state
+    return (
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, { loading, error }) => (
+          <Form
+            onSubmit={async e => {
+              //Stop form from submitting
+              e.preventDefault()
+              //call the mutation
+              const res = await createItem()
+              //reroute user to single item page of newly created item
+              Router.push({
+                pathname: '/item',
+                query: { id: res.data.createItem.id }
+              })
+            }}>
+            <Error error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  //   value={this.state.image}
+                  onChange={this.uploadFile}
+                />
+                {image && <img width="200" src={image} alt="Upload Preview" />}
+              </label>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  value={title}
+                  onChange={this.handleChange}
+                />
+              </label>
+
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  value={price}
+                  onChange={this.handleChange}
+                />
+              </label>
+
+              <label htmlFor="description">
+                Description
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter A Description"
+                  required
+                  value={description}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
+    )
+  }
+}
+
+export { CREATE_ITEM_MUTATION }
