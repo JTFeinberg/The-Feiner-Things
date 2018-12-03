31803299a2d2f4fe389d975ac6ec1ff43a5c01e2
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-03 13:22:42 -0500

    style(SingleItem): Write single item styled component

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
+import styled from 'styled-components'
+import Error from './ErrorMessage'
+
+const SingleItemStyles = styled.div`
+max-width: 1200px;
+margin: 2rem auto;
+box-shadow: ${props => props.theme.bs};
+display: grid;
+grid-auto-columns: 1fr;
+grid-auto-flow: column;
+min-height: 800px;
+`
 
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
@@ -18,8 +30,10 @@ export default class SingleItem extends Component {
         return (
             <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
                 {({ error, loading, data }) => {
-                    if (error) return <p>Error!</p>
+                    const { item } = data
+                    if (error) return <Error error={error} />
                     if (loading) return <p>Loading...</p>
+                    if (!data.item) return <p>No Item Found for ID {this.props.id}</p>
                     return <p>Single Item Component!!</p>
                 }}
             </Query>
