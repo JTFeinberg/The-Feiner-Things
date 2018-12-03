fe748d87b4949c8bdc819285dd42abf7e3524458
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-03 13:00:04 -0500

    feat(SingleItem): Query an items id and get single items info

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -1,4 +1,16 @@
 import React, { Component } from 'react'
+import gql from 'graphql-tag'
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: {id: $id}){
+        id
+        title
+        description
+        largeImage
+    }
+}
+`
 
 export default class SingleItem extends Component {
     render() {
