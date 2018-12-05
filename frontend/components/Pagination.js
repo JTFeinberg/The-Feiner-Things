ae162be7ecd73723ffeeaa12e15068ebce0ce449
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-05 15:03:27 -0500

    feat(Pagination): Write query to get a count of all items for pagination

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import gql from 'graphql-tag'
 import PaginationStyles from './styles/PaginationStyles'
+
+const PAGINATION_QUERY = gql`
+    query PAGINATION_QUERY {
+        itemsConnection {
+            aggregate {
+                count
+            }
+        }
+}
+`
 
 const Pagination = props => {
     return (
