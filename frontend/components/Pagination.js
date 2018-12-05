ee85e0569e91d0934f1ee85b4bb17e986f121ca6
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-05 15:17:26 -0500

    feat(Pagination): Add variables for total num of items and of pages needed

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -2,6 +2,7 @@ import React from 'react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
 import PaginationStyles from './styles/PaginationStyles'
+import { perPage } from '../config'
 
 const PAGINATION_QUERY = gql`
     query PAGINATION_QUERY {
@@ -17,7 +18,11 @@ const Pagination = props => {
     return (
         <PaginationStyles>
             <Query query={PAGINATION_QUERY}>
-                {({ data, loading, error }) => <p>I'm on the Pagination component!</p>}
+                {({ data, loading, error }) => {
+                    const { count } = data.itemsConnection.aggregate
+                    const pages = Math.ceil(count / perPage)
+                    return <p>Page 1 of {pages}</p>
+                }}
             </Query>
 
         </PaginationStyles>
