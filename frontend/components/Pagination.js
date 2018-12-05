35942d69ca014a012a702cef143fdfa9bfa0f6eb
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-05 14:47:38 -0500

    feat(Pagination): Scaffold out pagination component

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PaginationStyles from './styles/PaginationStyles'
+
+const Pagination = props => {
+    return (
+        <PaginationStyles>
+            <p>I'm on the Pagination component!</p>
+        </PaginationStyles>
+    )
+}
+
+export default Pagination
