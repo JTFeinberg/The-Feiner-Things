56ddd6668e13564662b2b2aa3fb5777cf21727e2
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-07 14:07:05 -0500

    feat(Signup): Add three inputs for email, name, and password

--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -5,11 +5,46 @@ import Form from './styles/Form'
 import Error from './ErrorMessage'
 
 export default class Signup extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: ''
+  }
   render() {
     return (
       <Form>
         <fieldset>
-            <h2>Signup for an account</h2>
+          <h2>Signup for an account</h2>
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={this.state.email}
+              onChange={this.saveToState}
+            />
+          </label>
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              value={this.state.name}
+              onChange={this.saveToState}
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              value={this.state.password}
+              onChange={this.saveToState}
+            />
+          </label>
         </fieldset>
       </Form>
     )
