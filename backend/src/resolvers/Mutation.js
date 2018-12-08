37403946c8c98073bd0c740d3978f311b05679bb
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-08 16:44:15 -0500

    feat(Mutation): signin resolver should throw error if email not found in db

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -68,6 +68,18 @@ const Mutations = {
     })
     //finally return user to browser
     return user
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    //1.check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } })
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`)
+    }
+    //2. check if their password is correct
+    //3. generate JWT
+    //4. set the cookie with the token
+    //5. return the user
+
   }
 }
 
