3c20f5ed154bcb8ed95b9631cdc09428ea5e5585
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-10 11:45:46 -0500

    feat(Signout): Write signout mutation

--- frontend/components/Signout.js
+++ frontend/components/Signout.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 
-// const SIGNOUT_USER_MUTATION = gql`
-//   mutation SIGNOUT_USER_MUTATION() {
-//       signout() {
-//           message
-//       }
-//   }
-// `
+const SIGN_OUT_MUTATION = gql`
+  mutation SIGN_OUT_MUTATION {
+    signout {
+      message
+    }
+  }
+`
 
 const Signout = props => <button>Sign Out</button>
 
