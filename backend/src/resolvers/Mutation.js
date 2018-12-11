59b176dd0187fe3bd26bb1efa6a7834654cef178
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-11 18:03:48 -0500

    feat(Mutation): Throw error is non user tries to create item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -9,6 +9,10 @@ const tokenAge = 1000 * 60 * 60 * 24 * 365 //1 year
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TO DO: Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must log in first to sell an item!')
+
+    }
     const item = await ctx.db.mutation.createItem(
       {
         data: {
