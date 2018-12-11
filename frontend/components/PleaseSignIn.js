b4bd5643e2dd903280dce3dc3dc2f72cdc717c09
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-11 18:35:32 -0500

    feat(PleaseSignIn): Set up component to check if user is signed in

---
+++ frontend/components/PleaseSignIn.js
@@ -0,0 +1,7 @@
+import { Query } from 'react-apollo'
+import { CURRENT_USER_QUERY } from './User'
+import Signin from './Signin'
+
+const PleaseSignIn = props => <p>Please Sign In!</p>
+
+export default PleaseSignIn
