23f776550bf2c9376f2ca22738d1ea7f7c59255b
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-11 18:51:18 -0500

    feat(PleaseSignIn): Display child components is user is signed in

--- frontend/components/PleaseSignIn.js
+++ frontend/components/PleaseSignIn.js
@@ -4,15 +4,17 @@ import Signin from './Signin'
 
 const PleaseSignIn = props => (
   <Query query={CURRENT_USER_QUERY}>
-    {(data, loading) => {
+    {({ data, loading }) => {
       if (loading) return <p>Loading...</p>
-      if (!data.me)
+      if (!data.me) {
         return (
           <div>
             <p>Please sign in before continuing</p>
             <Signin />
           </div>
         )
+      }
+      return props.children
     }}
   </Query>
 )
