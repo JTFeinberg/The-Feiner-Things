baa86364369f7d38893a937dde2dd2e31d77d6aa
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-11 10:22:59 -0500

    refactor(RequestReset): Remove and change code to reflect reset request

--- frontend/components/RequestReset.js
+++ frontend/components/RequestReset.js
@@ -3,42 +3,35 @@ import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 import Form from './styles/Form'
 import Error from './ErrorMessage'
-import { CURRENT_USER_QUERY } from './User'
 
-const SIGNIN_MUTATION = gql`
-  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
-    signin(email: $email, password: $password) {
-      id
-      email
-      name
+const REQUEST_RESET_MUTATION = gql`
+  mutation REQUEST_RESET_MUTATION($email: String!) {
+    requestReset(email: $email) {
+      message
     }
   }
 `
 
 export default class Signin extends Component {
   state = {
-    email: '',
-    password: ''
+    email: ''
   }
   saveToState = ({ target }) => {
     this.setState({ [target.name]: target.value })
   }
   render() {
     return (
-      <Mutation
-        mutation={SIGNIN_MUTATION}
-        variables={this.state}
-        refetchQueries={[{ query: CURRENT_USER_QUERY }]}>
-        {(signin, { error, loading }) => (
+      <Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
+        {(requestReset, { error, loading }) => (
           <Form
             method="post"
             onSubmit={async e => {
               e.preventDefault()
-              await signin()
-              this.setState({ email: '', password: '' })
+              await requestReset()
+              this.setState({ email: '' })
             }}>
             <fieldset disabled={loading} aria-busy={loading}>
-              <h2>Sign into your account</h2>
+              <h2>Request a password reset</h2>
               <Error error={error} />
               <label htmlFor="email">
                 Email
@@ -50,17 +43,7 @@ export default class Signin extends Component {
                   onChange={this.saveToState}
                 />
               </label>
-              <label htmlFor="password">
-                Password
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="password"
-                  value={this.state.password}
-                  onChange={this.saveToState}
-                />
-              </label>
-              <button type="submit">Sign In!</button>
+              <button type="submit">Request Reset!</button>
             </fieldset>
           </Form>
         )}
