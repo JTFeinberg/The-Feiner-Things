5d4e0ea0febb1abbd04eda2ba2b6df596b755ef1
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-11 18:08:03 -0500

    feat(Mutation): Save user id with item when created

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -8,10 +8,8 @@ const { transport, makeANiceEmail } = require('../mail')
 const tokenAge = 1000 * 60 * 60 * 24 * 365 //1 year
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // TO DO: Check if they are logged in
     if (!ctx.request.userId) {
       throw new Error('You must log in first to sell an item!')
-
     }
     const item = await ctx.db.mutation.createItem(
       {
