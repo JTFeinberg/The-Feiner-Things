f251d8826c5f2bb7d5a386b8f0d1ef41225715d5
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-11 18:46:34 -0500

    feat(PleaseSignIn): If no user, instruct them to sign in and display form

--- frontend/components/PleaseSignIn.js
+++ frontend/components/PleaseSignIn.js
@@ -6,6 +6,13 @@ const PleaseSignIn = props => (
   <Query query={CURRENT_USER_QUERY}>
     {(data, loading) => {
       if (loading) return <p>Loading...</p>
+      if (!data.me)
+        return (
+          <div>
+            <p>Please sign in before continuing</p>
+            <Signin />
+          </div>
+        )
     }}
   </Query>
 )
