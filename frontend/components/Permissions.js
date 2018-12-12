99bcf8d477f67ffbc51b5aff93d78618ce893fd2
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-12 15:32:04 -0500

    feat(Permissions): Handle any errors

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Query } from 'react-apollo'
+import Error from './ErrorMessage'
 
 const Permissions = props => (
-  <Query query={ALL_USERS_QUERY}>{({ data, loading, error }) => <p>Hey</p>}</Query>
+  <Query query={ALL_USERS_QUERY}>
+    {({ data, loading, error }) => (
+      <div>
+        <Error error={error} />
+        <p>Hey</p>
+      </div>
+    )}
+  </Query>
 )
