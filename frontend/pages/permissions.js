cc021e1fdab350cca7f14654d8ddc6bff9e48a44
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-12 15:25:50 -0500

    feat(permissions): Create permissions page

---
+++ frontend/pages/permissions.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PleaseSignIn from '../components/PleaseSignIn'
+
+const PermissionsPage = props => {
+  return (
+    <div>
+      <PleaseSignIn>
+        <p>Permissions!</p>
+      </PleaseSignIn>
+    </div>
+  )
+}
+
+export default PermissionsPage
