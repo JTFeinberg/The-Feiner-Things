56cd4a89a10f1d6dc9c38f034eff5dc769023beb
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-13 09:59:17 -0500

    chore(DeleteItem): Give button explicit type

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -17,9 +17,7 @@ export default class DeleteItem extends Component {
     //1. Read the cahce for the items we want
     const data = cache.readQuery({ query: ALL_ITEMS_QUERY })
     //2. Filter the deleted item out of the page
-    data.items = data.items.filter(
-      item => item.id !== payload.data.deleteItem.id
-    )
+    data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id)
     //3. Put the items back!
     cache.writeQuery({ query: ALL_ITEMS_QUERY, data })
   }
@@ -31,6 +29,7 @@ export default class DeleteItem extends Component {
         update={this.update}>
         {(deleteItem, { error }) => (
           <button
+            type="button"
             onClick={() => {
               if (confirm('Are you sure you want to delete this item?')) {
                 deleteItem()
