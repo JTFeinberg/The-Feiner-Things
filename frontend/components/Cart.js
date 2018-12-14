5a6d878d9b388abaf4b67c5730edccb08cf57370
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-14 11:05:03 -0500

    feat(Cart): Expose component to toggleCart mutation and call it on close button click

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -22,21 +22,27 @@ const TOGGLE_CART_MUTATION = gql`
 
 const Cart = () => {
   return (
-    <Query query={LOCAL_STATE_QUERY}>
-      {({ data }) => (
-        <CartStyles open={data.cartOpen}>
-          <header>
-            <CloseButton title="close">&times;</CloseButton>
-            <Supreme>Your Cart</Supreme>
-            <p>You have __ items in your cart</p>
-          </header>
-          <footer>
-            <p>$10.10</p>
-            <SickButton>Checkout</SickButton>
-          </footer>
-        </CartStyles>
+    <Mutation mutation={TOGGLE_CART_MUTATION}>
+      {toggleCart => (
+        <Query query={LOCAL_STATE_QUERY}>
+          {({ data }) => (
+            <CartStyles open={data.cartOpen}>
+              <header>
+                <CloseButton title="close" onClick={toggleCart}>
+                  &times;
+                </CloseButton>
+                <Supreme>Your Cart</Supreme>
+                <p>You have __ items in your cart</p>
+              </header>
+              <footer>
+                <p>$10.10</p>
+                <SickButton>Checkout</SickButton>
+              </footer>
+            </CartStyles>
+          )}
+        </Query>
       )}
-    </Query>
+    </Mutation>
   )
 }
 
