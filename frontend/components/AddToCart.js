5966da7a5ef7e3a75e8766bda8a9227c82a02b37
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-15 17:11:27 -0500

    feat(AddToCart): Write add to cart mutation on front end

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 
+const ADD_TO_CART_MUTATION = gql`
+  mutation ADD_TO_CART_MUTATION($id: ID!) {
+      AddToCart(id: $id) {
+          id
+          quantity
+      }
+  }
+`
+
 export default class AddToCart extends Component {
     render() {
         const { id } = this.props
