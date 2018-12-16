3e50646dde95717c442c499539b03968fb837fbf
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-16 14:10:43 -0500

    feat(CartItem): Make sure CartItem has a cartItem prop via prop types

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import formatMoney from '../lib/formatMoney'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 const CartItemStyles = styled.li``
 
-const CartItem = props => <CartItemStyles>HI</CartItemStyles>
+const CartItem = ({ cartItem }) => <CartItemStyles>{cartItem.id}</CartItemStyles>
+
+CartItem.propTypes = {
+  cartItem: PropTypes.object.isRequired
+}
 
 export default CartItem
