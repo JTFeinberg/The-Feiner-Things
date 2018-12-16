a4fc5e739b4e320f4819c6f3bfb709567a6b8a92
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-16 14:51:18 -0500

    style(CartItem): Add rules to CartItemStyles component

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -3,7 +3,20 @@ import formatMoney from '../lib/formatMoney'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-const CartItemStyles = styled.li``
+const CartItemStyles = styled.li`
+  padding: 1rem 0;
+  border-bottom: 1px solid ${props => props.theme.lightgrey};
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr auto;
+  img {
+    margin-right: 10px;
+  }
+  h3,
+  p {
+    margin: 0;
+  }
+`
 
 const CartItem = ({ cartItem }) => {
   const { image, title, price } = cartItem.item
@@ -16,7 +29,7 @@ const CartItem = ({ cartItem }) => {
           {formatMoney(price * cartItem.quantity)}
           {' - '}
           <em>
-            {cartItem.quantity} &times; {formatMoney(price)}
+            {cartItem.quantity} &times; {formatMoney(price)} each
           </em>
         </p>
       </div>
