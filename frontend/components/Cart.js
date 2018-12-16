64ff3819e07954e82eaa39915b856e10a8e4fc6e
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-16 13:51:35 -0500

    refactor(Cart): Display users name in cart

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -23,32 +23,33 @@ const TOGGLE_CART_MUTATION = gql`
 
 const Cart = () => {
   return (
-    <User>{({ data: { me } }) => {
-      if (!me) return null
-      return (
-        <Mutation mutation={TOGGLE_CART_MUTATION}>
-          {toggleCart => (
-            <Query query={LOCAL_STATE_QUERY}>
-              {({ data }) => (
-                <CartStyles open={data.cartOpen}>
-                  <header>
-                    <CloseButton title="close" onClick={toggleCart}>
-                      &times;
-                </CloseButton>
-                    <Supreme>Your Cart</Supreme>
-                    <p>You have __ items in your cart</p>
-                  </header>
-                  <footer>
-                    <p>$10.10</p>
-                    <SickButton>Checkout</SickButton>
-                  </footer>
-                </CartStyles>
-              )}
-            </Query>
-          )}
-        </Mutation>
-      )
-    }}
+    <User>
+      {({ data: { me } }) => {
+        if (!me) return null
+        return (
+          <Mutation mutation={TOGGLE_CART_MUTATION}>
+            {toggleCart => (
+              <Query query={LOCAL_STATE_QUERY}>
+                {({ data }) => (
+                  <CartStyles open={data.cartOpen}>
+                    <header>
+                      <CloseButton title="close" onClick={toggleCart}>
+                        &times;
+                      </CloseButton>
+                      <Supreme>{me.name}'s Cart</Supreme>
+                      <p>You have __ items in your cart</p>
+                    </header>
+                    <footer>
+                      <p>$10.10</p>
+                      <SickButton>Checkout</SickButton>
+                    </footer>
+                  </CartStyles>
+                )}
+              </Query>
+            )}
+          </Mutation>
+        )
+      }}
     </User>
   )
 }
