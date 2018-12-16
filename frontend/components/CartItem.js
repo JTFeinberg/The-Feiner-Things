aff88be0fb97a0176dc09961ef800949f2785ee0
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-16 14:05:30 -0500

    feat(CartItem): Scaffold out CartItem component

---
+++ frontend/components/CartItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import formatMoney from '../lib/formatMoney'
+import styled from 'styled-components'
+
+const CartItemStyles = styled.li``
+
+const CartItem = props => <CartItemStyles>HI</CartItemStyles>
+
+export default CartItem
