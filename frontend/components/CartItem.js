af25a16ca455c9888988d73da9d8cfbeeebda09d
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-16 14:29:25 -0500

    feat(CartItem): Display title, total, quantity, and price of each cart item

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import formatMoney from '../lib/formatMoney'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
@@ -6,10 +6,20 @@ import PropTypes from 'prop-types'
 const CartItemStyles = styled.li``
 
 const CartItem = ({ cartItem }) => {
-  const { image, title } = cartItem.item
+  const { image, title, price } = cartItem.item
   return (
     <CartItemStyles>
       <img width="100" src={image} alt={title} />
+      <div className="cart-item-details">
+        <h3>{title}</h3>
+        <p>
+          {formatMoney(price * cartItem.quantity)}
+          {' - '}
+          <em>
+            {cartItem.quantity} &times; {formatMoney(price)}
+          </em>
+        </p>
+      </div>
     </CartItemStyles>
   )
 }
