b5656eda2c7c4201d25fa150866ab488c0554406
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-17 18:38:44 -0500

    feat(RemoveFromCart): Write frontend remove from cart mutation

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -5,6 +5,14 @@ import PropTypes from 'prop-types'
 import gql from 'graphql-tag'
 import { CURRENT_USER_QUERY } from './User'
 
+const REMOVE_FROM_CART_MUTATION = gql`
+  mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
+    removeFromCart(id: $id) {
+      id
+    }
+  }
+`
+
 const BigButton = styled.button`
   font-size: 3rem;
   background: none;
