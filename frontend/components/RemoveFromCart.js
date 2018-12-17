3dc16aad87dd72629f6945eec1649cd38ea34e95
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-17 18:28:02 -0500

    style(RemoveFromCart): Add style rules to BigButton

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -5,10 +5,18 @@ import PropTypes from 'prop-types'
 import gql from 'graphql-tag'
 import { CURRENT_USER_QUERY } from './User'
 
-const BigButton = styled.button``
+const BigButton = styled.button`
+  font-size: 3rem;
+  background: none;
+  border: 0;
+  &:hover {
+    color: ${props => props.theme.red};
+    cursor: pointer;
+  }
+`
 
 export default class RemoveFromCart extends Component {
   render() {
-    return <BigButton>Hi</BigButton>
+    return <BigButton title="Delete Item">&times;</BigButton>
   }
 }
