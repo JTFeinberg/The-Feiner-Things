46e461ce3e30567983da2c5b909849d401f3dd65
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-19 09:22:57 -0500

    chore(CartCount): Import components from react transition group

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { TansitionGroup, CSSTransition } from 'react-transition-group'
 import styled from 'styled-components'
 
 const Dot = styled.div`
