585da77911653a805077dd229a6d1d220be076a4
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-21 03:18:01 -0500

    chore(Search): Import dependencies for Search component

---
+++ frontend/components/Search.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import DownShift from 'downshit'
+import Router from 'next/router'
+import { ApolloConsumer } from 'react adopt'
+import gql from 'graphql-tag'
+import debounce from 'lodash.debounce'
+import { Dropdown, DropDownItem, SearchStyles } from './styles/DropDown'
