9590f3ea22260471e05eee3b5e21538858e5dd6a
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-21 04:07:16 -0500

    feat(Search): Scaffold out AutoComplete component

--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 import DownShift from 'downshit'
 import Router from 'next/router'
 import { ApolloConsumer } from 'react adopt'
 import gql from 'graphql-tag'
 import debounce from 'lodash.debounce'
 import { Dropdown, DropDownItem, SearchStyles } from './styles/DropDown'
+
+export default class AutoComplete extends Component {
+  render() {
+    return (
+      <SearchStyles>
+        <div>
+          <input type="search" />
+          <Dropdown>
+            <p>Items will go here</p>
+          </Dropdown>
+        </div>
+      </SearchStyles>
+    )
+  }
+}
