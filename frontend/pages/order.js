ebf2558b69f731fa55459eaebcacafa49fea2dc7
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-26 18:52:25 +0100

    feat(order): Scaffold out an order page

---
+++ frontend/pages/order.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PleaseSignIn from '../components/PleaseSignIn'
+
+const Order = props => {
+  return (
+    <div>
+      <PleaseSignIn>
+        <p>This is a single order</p>
+      </PleaseSignIn>
+    </div>
+  )
+}
+
+export default Order
