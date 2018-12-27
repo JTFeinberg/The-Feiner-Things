42c74727daaad7024a782ef17b3e567288f9a86e
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-12-27 08:42:02 +0100

    chore(Order): Import dependencies

--- frontend/components/Order.js
+++ frontend/components/Order.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Query } from 'react-apollo'
+import { format } from 'date-fns'
+import Head from 'next/head'
+import gql from 'graphql-tag'
+import formatMoney from '../lib/formatMoney'
+import Error from './ErrorMessage'
+import OrderStyles from './styles/OrderStyles'
 
 export default class Order extends Component {
   static propTypes = {
