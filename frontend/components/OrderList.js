453d557c63a0ed0ae93ddb145b9f6861bc28c7ba
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-02 08:52:09 -0500

    feat(OrderList): Define the orders query on the client side

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -7,6 +7,24 @@ import gql from 'graphql-tag'
 import formatMoney from '../lib/formatMoney'
 import OrderItemStyles from './styles/OrderItemStyles'
 
+const USER_ORDERS_QUERY = gql`
+  query USER_ORDERS_QUERY {
+    orders(orderBy: createdAt_DESC) {
+      id
+      total
+      createdAt
+      item {
+        id
+        title
+        price
+        description
+        quantity
+        image
+      }
+    }
+  }
+`
+
 export default class OrderList extends Component {
   render() {
     return (
