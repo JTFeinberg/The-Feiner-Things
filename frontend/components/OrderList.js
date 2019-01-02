28c03974913e23959e84f75802369b00e545fc4d
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-02 08:42:47 -0500

    chore(OrderList): Import dependencies

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react'
+import { Query } from 'react-apollo'
+import { formatDistance } from 'date-fns'
+import Link from 'next/link'
+import styled from 'styled-components'
+import gql from 'graphql-tag'
+import formatMoney from '../lib/formatMoney'
+import OrderItemStyles from './styles/OrderItemStyles'
 
 export default class OrderList extends Component {
   render() {
