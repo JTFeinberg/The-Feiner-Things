9709ac5bbfe806b3bcd834ce434d95567b0d456b
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-02 09:11:26 -0500

    feat(OrderList): Loop over each order and link to specific ones

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -26,7 +26,7 @@ const USER_ORDERS_QUERY = gql`
   }
 `
 
-const orderUl = styled.ul`
+const OrderUl = styled.ul`
   display: grid;
   grid-gap: 4rem;
   grid-template-columns: repeat(auto-fit, minMax(40%, 1fr));
@@ -39,10 +39,22 @@ export default class OrderList extends Component {
         {({ data: { orders }, loading, error }) => {
           if (loading) return <p>Loading...</p>
           if (error) return <Error error={error} />
-          console.log(orders)
           return (
             <div>
               <h2>You have {orders.length} orders</h2>
+              <OrderUl>
+                {orders.map(order => (
+                  <OrderItemStyles key={order.id}>
+                    <Link
+                      href={{
+                        pathname: '/order',
+                        query: { id: order.id }
+                      }}>
+                      <a>hi</a>
+                    </Link>
+                  </OrderItemStyles>
+                ))}
+              </OrderUl>
             </div>
           )
         }}
