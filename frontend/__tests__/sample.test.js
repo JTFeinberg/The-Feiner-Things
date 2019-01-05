a82ec5f7183be69c91645a29397481e5112d811a
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-05 09:49:10 -0500

    test(sample): Write more it blocks tests

--- frontend/__tests__/sample.test.js
+++ frontend/__tests__/sample.test.js
@@ -2,6 +2,10 @@ describe('sample test 101', () => {
   it('works as expected', () => {
     const age = 100
     expect(1).toEqual(1)
-    expect(age).toEqual(99)
+    expect(age).toEqual(100)
+  })
+  it('Handles ranges', () => {
+    const age = 200
+    expect(age).toBeGreaterThan(100)
   })
 })
