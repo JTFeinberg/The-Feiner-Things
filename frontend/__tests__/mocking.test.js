d46b7f4560045b0167fa6ef5078da204e0fbaa95
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-06 11:28:59 -0500

    test(mocking): Create function to practice testing api calls

--- frontend/__tests__/mocking.test.js
+++ frontend/__tests__/mocking.test.js
@@ -1,3 +1,15 @@
+function Person(name, foods) {
+  this.name = name
+  this.foods = foods
+}
+
+Person.prototype.fetchFavFoods = () => {
+  return new Promise((resolve, reject) => {
+    //   simulate an API
+    setTimeout(() => resolve(this.foods), 2000)
+  })
+}
+
 describe('mocking learning', () => {
   it('mocks a reg function', () => {
     const fetchDogs = jest.fn()
