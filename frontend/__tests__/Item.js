2dd161ff1cca1d37a4b7620857d58d6edc12d257
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-06 11:49:07 -0500

    test(Item): Import component and create fake item for testing

---
+++ frontend/__tests__/Item.js
@@ -0,0 +1,10 @@
+import ItemComponent from '../components/Item'
+
+const fakeItem = {
+  id: 'ABC123',
+  title: 'A Cool Item',
+  price: 5000,
+  description: 'This is a really cool item!',
+  image: 'item.jpg',
+  largeImage: 'largeItem.jpg'
+}
