348a85e0ce5a48ab71c73120ab53aabf27b434a4
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-07 10:45:46 -0500

    test(Item): Move wrapper to higher scope for dryness

--- frontend/__tests__/Item.js
+++ frontend/__tests__/Item.js
@@ -12,17 +12,22 @@ const fakeItem = {
 }
 
 describe('<Item />', () => {
+  const wrapper = shallow(<ItemComponent item={fakeItem} />)
+
   it('renders the price tage and title properly', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem} />)
     const PriceTag = wrapper.find('PriceTag')
     expect(PriceTag.children().text()).toBe('$50')
     expect(wrapper.find('Title a').text()).toBe(fakeItem.title)
   })
 
   it('renders the image properly', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem} />)
+    // const wrapper = shallow(<ItemComponent item={fakeItem} />)
     const img = wrapper.find('img')
     expect(img.props().src).toBe(fakeItem.image)
     expect(img.props().alt).toBe(fakeItem.title)
+  })
+
+  it('renders out the buttons properly', () => {
+    const buttonList = wrapper.find('.buttonList')
   })
 })
