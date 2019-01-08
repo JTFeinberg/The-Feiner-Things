6a27570f420af5efbee5c1e86e44933fd9aa4339
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-08 16:25:44 -0500

    test(SingleItem): Mount component with mocked provider in test

--- frontend/__tests__/SingleItem.test.js
+++ frontend/__tests__/SingleItem.test.js
@@ -5,3 +5,13 @@ import wait from 'waait'
 import { MockedProvider } from 'react-apollo/test-utils'
 import SingleItem, { SINGLE_ITEM_QUERY } from '../components/SingleItem'
 import { fakeItem } from '../lib/testUtils'
+
+describe('<SingleItem />', () => {
+    if('renders with proper data', async () => {
+        const wrapper = mount(
+            <MockedProvider>
+                <SingleItem id='123'/>
+            </MockedProvider>
+        )
+    })
+})
