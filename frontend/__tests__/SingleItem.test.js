7fffe4febe1d7edeb9642ecc23f3612617853662
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-08 16:29:28 -0500

    test(SingleItem): Pass mocked provider some mocked data

--- frontend/__tests__/SingleItem.test.js
+++ frontend/__tests__/SingleItem.test.js
@@ -7,11 +7,23 @@ import SingleItem, { SINGLE_ITEM_QUERY } from '../components/SingleItem'
 import { fakeItem } from '../lib/testUtils'
 
 describe('<SingleItem />', () => {
-    if('renders with proper data', async () => {
-        const wrapper = mount(
-            <MockedProvider>
-                <SingleItem id='123'/>
-            </MockedProvider>
-        )
-    })
+  it('renders with proper data', async () => {
+    const mocks = [
+      {
+        //when somebody makes a request with this query and variable combo
+        request: { query: SINGLE_ITEM_QUERY, variables: { id: '123' } },
+        //return this fake data (mocked data)
+        result: {
+          data: {
+            item: fakeItem()
+          }
+        }
+      }
+    ]
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <SingleItem id="123" />
+      </MockedProvider>
+    )
+  })
 })
