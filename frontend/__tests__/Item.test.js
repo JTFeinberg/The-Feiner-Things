96d8618447684475e347da2345ce61cc84404b80
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-08 10:03:38 -0500

    test(Item): Practice creating a snapshot of component

--- frontend/__tests__/Item.test.js
+++ frontend/__tests__/Item.test.js
@@ -12,25 +12,29 @@ const fakeItem = {
 }
 
 describe('<Item />', () => {
-  const wrapper = shallow(<ItemComponent item={fakeItem} />)
-
-  it('renders the price tage and title properly', () => {
-    const PriceTag = wrapper.find('PriceTag')
-    expect(PriceTag.children().text()).toBe('$50')
-    expect(wrapper.find('Title a').text()).toBe(fakeItem.title)
+  it('renders and matches the snapshot', () => {
+    const price = '$50.34'
+    expect(price).toMatchSnapshot()
   })
+  // const wrapper = shallow(<ItemComponent item={fakeItem} />)
 
-  it('renders the image properly', () => {
-    const img = wrapper.find('img')
-    expect(img.props().src).toBe(fakeItem.image)
-    expect(img.props().alt).toBe(fakeItem.title)
-  })
+  // it('renders the price tage and title properly', () => {
+  //   const PriceTag = wrapper.find('PriceTag')
+  //   expect(PriceTag.children().text()).toBe('$50')
+  //   expect(wrapper.find('Title a').text()).toBe(fakeItem.title)
+  // })
 
-  it('renders out the buttons properly', () => {
-    const buttonList = wrapper.find('.buttonList')
-    expect(buttonList.children()).toHaveLength(3)
-    expect(buttonList.find('Link')).toHaveLength(1)
-    expect(buttonList.find('AddToCart').exists()).toBe(true)
-    expect(buttonList.find('DeleteItem').exists()).toBe(true)
-  })
+  // it('renders the image properly', () => {
+  //   const img = wrapper.find('img')
+  //   expect(img.props().src).toBe(fakeItem.image)
+  //   expect(img.props().alt).toBe(fakeItem.title)
+  // })
+
+  // it('renders out the buttons properly', () => {
+  //   const buttonList = wrapper.find('.buttonList')
+  //   expect(buttonList.children()).toHaveLength(3)
+  //   expect(buttonList.find('Link')).toHaveLength(1)
+  //   expect(buttonList.find('AddToCart').exists()).toBe(true)
+  //   expect(buttonList.find('DeleteItem').exists()).toBe(true)
+  // })
 })
