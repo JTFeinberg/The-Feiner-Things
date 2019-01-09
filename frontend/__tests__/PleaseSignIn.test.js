f0a7ba1d4548df2fcbd73387311db83132b3974a
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-09 08:30:07 -0500

    test(PleaseSignIn): Import necessary dependencies

---
+++ frontend/__tests__/PleaseSignIn.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import toJSON from 'enzyme-to-json'
+import wait from 'waait'
+import { MockedProvider } from 'react-apollo/test-utils'
+import PleaseSignIn from '../components/PleaseSignIn'
+import { CURRENT_USER_QUERY } from '../components/User '
+import { fakeUser } from '../lib/testUtils'
