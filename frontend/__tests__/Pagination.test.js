e3289d15ba5abdd752b6b6eddc9346b0abb6292c
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-11 10:06:52 -0500

    test(Pagination): Create returns mocked data for pagination

--- frontend/__tests__/Pagination.test.js
+++ frontend/__tests__/Pagination.test.js
@@ -4,3 +4,22 @@ import toJSON from 'enzyme-to-json'
 import wait from 'waait'
 import { MockedProvider } from 'react-apollo/test-utils'
 import Pagination, { PAGINATION_QUERY } from '../components/Pagination'
+
+function makeMocksFor(length) {
+  return [
+    {
+      request: { query: PAGINATION_QUERY, variables: { skip: 0, first: 4 } },
+      result: {
+        data: {
+          itemsConnection: {
+            __typename: 'aggregate',
+            aggregate: {
+              __typename: 'count',
+              count: length
+            }
+          }
+        }
+      }
+    }
+  ]
+}
