4c76964b3e8c056689740a55ae6174418ae78173
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-11 11:18:17 -0500

    test(Pagination): Create a mock router

--- frontend/__tests__/Pagination.test.js
+++ frontend/__tests__/Pagination.test.js
@@ -3,8 +3,13 @@ import { mount } from 'enzyme'
 import toJSON from 'enzyme-to-json'
 import wait from 'waait'
 import { MockedProvider } from 'react-apollo/test-utils'
+import Router from 'next/router'
 import Pagination, { PAGINATION_QUERY } from '../components/Pagination'
 
+Router.router = {
+  push() {},
+  prefetch() {}
+}
 function makeMocksFor(length) {
   return [
     {
@@ -34,5 +39,14 @@ describe('<Pagination />', () => {
     expect(wrapper.text()).toContain('Loading...')
     // const pagination = wrapper.find('[data-test="pagination"]')
     // expect(toJSON(pagination)).toMatchSnapshot()
+  })
+  it('renders pagination for 18 items', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={makeMocksFor(18)}>
+        <Pagination page={1} />
+      </MockedProvider>
+    )
+    await wait()
+    wrapper.update()
   })
 })
