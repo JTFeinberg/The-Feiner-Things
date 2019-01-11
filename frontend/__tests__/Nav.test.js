8e6af865c3246265f9a21456dfb6c4b4bacccc44
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-11 09:05:59 -0500

    test(Nav): Add mocks for signed in and not signed in states

--- frontend/__tests__/Nav.test.js
+++ frontend/__tests__/Nav.test.js
@@ -6,3 +6,17 @@ import { MockedProvider } from 'react-apollo/test-utils'
 import Nav from '../components/Nav'
 import { CURRENT_USER_QUERY } from '../components/User'
 import { fakeUser } from '../lib/testUtils'
+
+const notSignedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: null } }
+  }
+]
+
+const signedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: fakeUser() } }
+  }
+]
