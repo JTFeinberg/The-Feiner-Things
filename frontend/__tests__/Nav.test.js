68ced4ae7b33bfa1fdbe946f704eeb04108a5087
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-11 09:04:36 -0500

    test(Nav): Import dependencies

---
+++ frontend/__tests__/Nav.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import toJSON from 'enzyme-to-json'
+import wait from 'waait'
+import { MockedProvider } from 'react-apollo/test-utils'
+import Nav from '../components/Nav'
+import { CURRENT_USER_QUERY } from '../components/User'
+import { fakeUser } from '../lib/testUtils'
