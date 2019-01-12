5037dcc1aec268487f5634a2bda8f41e1aa3373b
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-12 09:44:48 -0500

    test(RequestReset): Create mock data for testing mutation

--- frontend/__tests__/RequestReset.test.js
+++ frontend/__tests__/RequestReset.test.js
@@ -4,3 +4,16 @@ import toJSON from 'enzyme-to-json'
 import wait from 'waait'
 import { MockedProvider } from 'react-apollo/test-utils'
 import RequestReset, { REQUEST_RESET_MUTATION } from '../components/RequestReset'
+import { getMaxListeners } from 'cluster'
+
+const mocks = [
+  {
+    request: {
+      query: REQUEST_RESET_MUTATION,
+      variables: { email: 'JacobFeinberg92@gmail.com' }
+    },
+    result: {
+      data: { requestReset: { message: 'success', __typename: 'Message' } }
+    }
+  }
+]
