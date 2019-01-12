04b31dd863978e66290b0e1be73150cf9b559a7c
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-12 09:41:16 -0500

    test(RequestReset): Import dependencies

---
+++ frontend/__tests__/RequestReset.test.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import toJSON from 'enzyme-to-json'
+import wait from 'waait'
+import { MockedProvider } from 'react-apollo/test-utils'
+import RequestReset, { REQUEST_RESET_MUTATION } from '../components/RequestReset'
