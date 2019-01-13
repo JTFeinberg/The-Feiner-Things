248e49a8833592fcb2732fcc63c0ff00c810ef03
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-13 18:02:37 -0500

    test(AddtoCart): Create mock data for testing

--- frontend/__tests__/AddToCart.test.js
+++ frontend/__tests__/AddToCart.test.js
@@ -7,3 +7,31 @@ import { ApolloConsumer } from 'react-apollo'
 import AddToCart, { ADD_TO_CART_MUTATION } from '../components/AddToCart'
 import { CURRENT_USER_QUERY } from '../components/User'
 import { fakeUser, fakeCartItem } from '../lib/testUtils'
+
+const mocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        me: {
+          ...fakeUser(),
+          cart: []
+        }
+      }
+    }
+  },
+  {
+    request: {
+      query: ADD_TO_CART_MUTATION,
+      variables: { id: 'abc123' }
+    },
+    result: {
+      data: {
+        addToCart: {
+          ...fakeCartItem(),
+          quantity: 1
+        }
+      }
+    }
+  }
+]
