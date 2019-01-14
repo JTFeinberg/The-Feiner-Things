cbc860ff5ae01c45c92846d186e08a214507c10b
Jacob Feinberg <JacobFeinberg92@gmail.com>
2019-01-13 19:11:49 -0500

    test(RemoveFromCart): The component renders

--- frontend/__tests__/RemoveFromCart.test.js
+++ frontend/__tests__/RemoveFromCart.test.js
@@ -7,6 +7,7 @@ import { ApolloConsumer } from 'react-apollo'
 import RemoveFromCart, { REMOVE_FROM_CART_MUTATION } from '../components/RemoveFromCart'
 import { CURRENT_USER_QUERY } from '../components/User'
 import { fakeUser, fakeCartItem } from '../lib/testUtils'
+import { wrap } from 'module'
 
 const mocks = [
   {
@@ -37,3 +38,30 @@ const mocks = [
     }
   }
 ]
+
+describe('<RemoveFromCart />', () => {
+  it('renders and matches the snapshot', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <RemoveFromCart id="abc123" />
+      </MockedProvider>
+    )
+    expect(toJSON(wrapper.find('button'))).toMatchSnapshot()
+  })
+  it('removes the item from cart', async () => {
+    let apolloClient
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <ApolloConsumer>
+          {client => {
+            apolloClient = client
+            return <RemoveFromCart id="abc123" />
+          }}
+        </ApolloConsumer>
+      </MockedProvider>
+    )
+    const res = await apolloClient.query({ query: CURRENT_USER_QUERY })
+    expect(res.data.me.cart).toHaveLength(1)
+    expect(res.data.me.cart[0].item.price).toBe(5000)
+  })
+})
